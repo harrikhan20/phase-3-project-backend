ebd128802752100ac0f0dfa513050d52c7430915
harris <khanharris7@yahoo.com>
2022-03-16 16:02:37 -0500

    added additional validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,7 @@ class Movie < ActiveRecord::Base
  has_many :reviews, dependent: :destroy
  validates :title, presence: true
  validates :genre, presence: true, uniqueness: true
+ validates :plot, presence: true
+ validates :movie_length, presence: true
+ validates :actor_list, presence: true
 end
