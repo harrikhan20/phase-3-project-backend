573d8a606f38799d036b96a6fd477012275c095d
harris <khanharris7@yahoo.com>
2022-03-17 19:06:26 -0500

    added reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,28 +2,48 @@ class ReviewsController < ApplicationController
 
   # GET: /reviews
   get "/reviews" do
-    
+    Review.all.to_json(include: :movie)
   end
 
 
   # POST: /reviews
   post "/reviews" do
-    
+    reviews = Review.create(params)
+    if reviews.id
+      reviews.to_json(include: :movie)
+    else
+      reviews.errors.full_messages.to_sentence
+    end
   end
 
   # GET: /reviews/5
   get "/reviews/:id" do
-    
+    reviews = Review.find_by(id: params["id"])
+    if reviews
+      reviews.to_json(include: :movie)
+      else
+        {errors: "THIS REVIEW WAS NOT FOUND"}.to_json
   end
+end
 
 
   # PATCH: /reviews/5
   patch "/reviews/:id" do
-   
+    reviews = Review.find_by_id(params["id"])
+    if reviews && reviews.update(params)
+      reviews.to_json(include: :movie)
+    else
+      {errors: "THIS REVIEW COULD NOT BE UPDATED"}.to_json
+    end
   end
 
   # DELETE: /reviews/5/delete
   delete "/reviews/:id/delete" do
-    
+    reviews = Review.find_by_id(params["id"])
+    if reviews
+      reviews.destroy
+      else
+        {errors: "THIS REVIEW COULD NOT BE DELETED"}.to_json
+      end
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
     belongs_to :movie
+    validates :text, presence: true
+    validates :rating, presence: true
 end
