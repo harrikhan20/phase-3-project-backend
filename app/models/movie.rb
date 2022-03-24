bdc3e3e0da12ee2aa46a4b2eb2fe1dca976de7e4
harris <khanharris7@yahoo.com>
2022-03-23 19:21:40 -0500

    working on controllers

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,5 @@
 class Movie < ActiveRecord::Base
  has_many :reviews, dependent: :destroy
- validates :title, presence: true
- validates :genre, presence: true, uniqueness: true
- validates :plot, presence: true
- validates :movie_length, presence: true
- validates :actor_list, presence: true
+ validates :title, :genre, :plot, :movie_length, :actor_list,  presence: true
+ 
 end
